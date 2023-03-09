39cab19c0c77b6e4aa425193a5b33a69d36a9443
agnaldoanjos <agnaldo.anjos@gmail.com>
2023-03-08 23:31:08 -0300

    Código inicial

---
+++ jdbc-template/code/src/main/java/com/home/examples/SpringBootJdbcTemplateApplication.java
@@ -0,0 +1,26 @@
+package com.home.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class SpringBootJdbcTemplateApplication {
+
+    @Bean
+    JdbcTemplate getJdbcTemplate(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+        jdbcTemplate.setQueryTimeout(20); //20 seconds
+        jdbcTemplate.setFetchSize(10);  //fetch 10 rows at a time
+        return jdbcTemplate;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJdbcTemplateApplication.class, args);
+    }
+
+}
